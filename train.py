029a233d1dfd7a4140e49af1485756571ce586ce
windows_on_wsl <otomijuf.004@gmail.com>
2023-09-04 23:07:53 +0900

    fix

--- train.py
+++ train.py
@@ -17,8 +17,11 @@ def train_as_dataset(tokenizer, trainer, dataset, batch_size):
     def batch_iterator():
         for i in range(0, len(dataset), batch_size):
             if i + batch_size > len(dataset):
+                print(i + batch_size > len(dataset), i + batch_size, len(dataset))
+                print(dataset[i : len(dataset)-2]["text"])
                 yield dataset[i : len(dataset)-1]["text"]
-            yield dataset[i : i + batch_size]["text"]
+            else:
+                yield dataset[i : i + batch_size]["text"]
     print('len: ', len(dataset))
     tokenizer.train_from_iterator(batch_iterator(), trainer=trainer, length=len(dataset))
     return tokenizer
